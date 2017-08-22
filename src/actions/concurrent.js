38c7a806e1ca7fe5a8930e373b3b2047e93a5d95
Justin Moser <jmmoser15@gmail.com>
2017-08-22 17:29:48 -0500

    Commented all code

--- src/actions/concurrent.js
+++ src/actions/concurrent.js
@@ -1,42 +1,42 @@
-'use strict';
-
-const EventEmitter = require('events');
-
-class ConcurrentAction extends EventEmitter {
-  constructor(actions) {
-    super();
-    this._actions = actions;
-    this._activesLength = actions.length;
-  }
-
-  activate(time) {
-    this._startTime = time;
-    for (let i = 0; i < this._actionsLength; i++) {
-      this._actions[i].activate(time);
-    }
-  }
-
-  active(time) {
-    for (let i = 0; i < this._actionsLength; i++) {
-      if (this._actions[i].active(time)) {
-        return 1;
-      }
-    }
-    return 0;
-  }
-
-  tick(time) {
-    // let values = [];
-    // for (let i = 0; i < this._actionsLength; i++) {
-    //   values.push(this._actions[i].tick(tick));
-    // }
-    // this.emit('value', values);
-    // return value;
-
-    for (let i = 0; i < this._actionsLength; i++) {
-      this._actions[i].tick(tick);
-    }
-  }
-}
-
-module.exports = ConcurrentAction;
+// 'use strict';
+//
+// const EventEmitter = require('events');
+//
+// class ConcurrentAction extends EventEmitter {
+//   constructor(actions) {
+//     super();
+//     this._actions = actions;
+//     this._activesLength = actions.length;
+//   }
+//
+//   activate(time) {
+//     this._startTime = time;
+//     for (let i = 0; i < this._actionsLength; i++) {
+//       this._actions[i].activate(time);
+//     }
+//   }
+//
+//   active(time) {
+//     for (let i = 0; i < this._actionsLength; i++) {
+//       if (this._actions[i].active(time)) {
+//         return 1;
+//       }
+//     }
+//     return 0;
+//   }
+//
+//   tick(time) {
+//     // let values = [];
+//     // for (let i = 0; i < this._actionsLength; i++) {
+//     //   values.push(this._actions[i].tick(tick));
+//     // }
+//     // this.emit('value', values);
+//     // return value;
+//
+//     for (let i = 0; i < this._actionsLength; i++) {
+//       this._actions[i].tick(tick);
+//     }
+//   }
+// }
+//
+// module.exports = ConcurrentAction;
